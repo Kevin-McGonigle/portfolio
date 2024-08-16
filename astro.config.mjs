4c6dd726495490e468180d4bb047ba143ad0363b
Kevin McGonigle <kevin.mcgonigle@wetransfer.com>
2024-08-16 21:58:40 +0100

    Switch to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import solidJs from "@astrojs/solid-js"
 import tailwind from "@astrojs/tailwind"
-import vercel from "@astrojs/vercel/serverless"
+import vercel from "@astrojs/vercel/static"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   integrations: [mdx(), react(), solidJs(), tailwind()],
-  output: "server",
+  output: "static",
 })
