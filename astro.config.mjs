53cd07fd7c3fc4aa3c601ca4502d3780ea53d57b
Kevin McGonigle <kevin@bogpeople.com>
2025-05-27 15:21:26 +0100

    update astro tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
 import vercel from "@astrojs/vercel/static"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [
-    mdx(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [mdx(), react()],
   output: "static",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
