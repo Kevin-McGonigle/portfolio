9fc1df4441030622d0b6635bcf190454f0dd73d2
Kevin McGonigle <kevin@bogpeople.com>
2025-05-28 08:50:14 +0100

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,17 @@ import js from "@eslint/js"
 import astro from "eslint-plugin-astro"
 import prettier from "eslint-plugin-prettier/recommended"
 
-export default [js.configs.recommended, ...astro.configs.recommended, prettier]
+export default [
+  js.configs.recommended,
+  ...astro.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+        sourceType: "module",
+      },
+    },
+  },
+]
