cebb2d7e7fc08e04f449047dfcdb26dbd6b4eaae
Kevin McGonigle <kevin@bogpeople.com>
2025-10-22 22:21:47 +0100

    Remove house-sitting pages & fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
+import { includeIgnoreFile } from "@eslint/compat"
 import js from "@eslint/js"
 import astro from "eslint-plugin-astro"
 import prettier from "eslint-plugin-prettier/recommended"
+import { defineConfig } from "eslint/config"
+import { fileURLToPath, URL } from "node:url"
 
-export default [
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url))
+
+export default defineConfig([
+  includeIgnoreFile(gitignorePath),
   js.configs.recommended,
   ...astro.configs.recommended,
   prettier,
@@ -15,4 +21,4 @@ export default [
       },
     },
   },
-]
+])
