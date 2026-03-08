51d9a249d34c77747608f37cbc25bad34a0d4337
Kevin McGonigle <66637774+Kevin-McGonigle@users.noreply.github.com>
2026-03-08 23:12:41 +0000

    Add house-sitting guide (#44)
    * Add house-sitting guide
    
    * Add environment variables for build CI job

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,21 @@ import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
 import vercel from "@astrojs/vercel/static"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from "astro/config"
+import { defineConfig, envField } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  env: {
+    schema: {
+      ADDRESS: envField.string({ context: "client", access: "public" }),
+      ADDRESS_MAP_URL: envField.string({ context: "client", access: "public" }),
+      SPARE_KEY_LOCATION: envField.string({
+        context: "client",
+        access: "public",
+      }),
+    },
+  },
   integrations: [mdx(), react()],
   output: "static",
   vite: {
